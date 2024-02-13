699cdfad682d7ae5246f6eaa7983509be9a024ff
Mahesh Dattatray Nehere <maheshnehere93@gmail.com>
2024-02-14 01:49:29 +0530

    Update Git.java
    By asking how are you?

--- src/com/bridgelabz/Git.java
+++ src/com/bridgelabz/Git.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("How are you?");
     }
 }
