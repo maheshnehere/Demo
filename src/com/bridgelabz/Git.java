f59c053bc7c2c76d507f5c52b4738647236d2c6f
Mahesh Dattatray Nehere <maheshnehere93@gmail.com>
2024-02-14 08:50:23 +0530

    added number reverse for conflict

--- src/com/bridgelabz/Git.java
+++ src/com/bridgelabz/Git.java
@@ -4,5 +4,8 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("How are you?");
+
+        int number =9876;
+            
     }
 }
