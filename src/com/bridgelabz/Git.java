c337ed9c78f4cd5fa74b0ab73125214bb043526a
maheshnehere <maheshnehere93@gmail.com>
2024-02-14 08:51:06 +0530

    Added number 1234 for conflict in local

--- src/com/bridgelabz/Git.java
+++ src/com/bridgelabz/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("How are you?");
+
+        int number = 1234;
     }
 }
